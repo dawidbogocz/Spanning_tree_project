8b72de054cac0ff615aab333d2fa842a071832c3
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-11-07 14:36:08 +0100

    Class with first function to read data from input_file.txt

--- Project/Project/Source1.cpp
+++ Project/Project/Source1.cpp
@@ -3,7 +3,58 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 #include <set>
+
+class Graph{
+	private:
+		std::vector<std::pair<double,std::pair<int,int>>> edges;
+		std::vector<std::vector<double>> contents;
+	public:
+		void read(std::string input);
+};
+
+void Graph::read(std::string input){
+	std::string line;
+	std::string part;
+	std::vector<double> row;
+	
+	double rows;
+
+	std::ifstream file(input);
+	if (file.is_open()){
+		while(file.good()){
+			getline(file, line);
+			std::stringstream ss(line);
+			while(ss){
+				getline(ss, part, '(');
+				getline(ss, part, ',');
+				row.push_back(std::stoi(part));
+				getline(ss, part, ',');
+				row.push_back(std::stoi(part));
+				getline(ss, part, ')');
+				row.push_back(std::stoi(part));
+				getline(ss, part, ',');
+				contents.push_back(row);
+			}
+		}
+
+		rows=contents.size();
+
+		int node1, node2;
+		double weight;
+
+		for(int i=0; i<rows; i++){
+			node1=contents[i][0];
+			node2=contents[i][1];
+			weight=contents[i][2];
+			edges.push_back(std::make_pair(weight,std::make_pair(node1,node2)));
+		}
+	}else{
+		std::cout<< "there is no file with that name" <<std::endl;
+	}
+	file.close();
+}
 
 int count(std::string input){
 	std::string line;
@@ -31,7 +82,6 @@ int count(std::string input){
     int u=unique.size();
 	return u;
 }
-
 
 void usage(){
 	std::cout<<"Options: "<<std::endl;
